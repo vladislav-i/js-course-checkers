eb6298e20eb81f3ef6bb1f65136e222a14ea529a
Vlad <vlad@vlad.local>
2015-01-26 15:51:27 -0800

    adding files

---
+++ src/checkers.js
@@ -0,0 +1,24 @@
+var board, currentPlayer;
+
+var resetBoard = function () {
+  board = [
+    [' X ', 'wht', ' X ', 'wht', ' X ', 'wht', ' X ', 'wht'],
+    ['wht', ' X ', 'wht', ' X ', 'wht', ' X ', 'wht', ' X '],
+    [' X ', 'wht', ' X ', 'wht', ' X ', 'wht', ' X ', 'wht'],
+    [' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X '],
+    [' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X ', ' X '],
+    ['red', ' X ', 'red', ' X ', 'red', ' X ', 'red', ' X '],
+    [' X ', 'red', ' X ', 'red', ' X ', 'red', ' X ', 'red'],
+    ['red', ' X ', 'red', ' X ', 'red', ' X ', 'red', ' X ']
+  ];
+
+  currentPlayer = 'wht';
+
+  $(document).trigger('boardChange');
+};
+
+var selectSquare = function(row, col) {
+  if (board[row][col] === ' X ') {
+    $(document).trigger('invalidMove', "You selected an empty spot.");
+  }
+};
---
+++ src/console_driver.js
@@ -0,0 +1,20 @@
+var numToChar = ["a", "b", "c", "d", "e", "f", "g", "h"];
+var charToNum = {
+  a: 0,
+  b: 1,
+  c: 2,
+  d: 3,
+  e: 4,
+  f: 5,
+  g: 6,
+  h: 7
+}
+
+var displayBoard = function () {
+  var column = [0, 1, 2, 3, 4, 5, 6, 7];
+  console.log("  | " + column.join("   "));
+  console.log("-----------------------------------");
+  for (var i = 0; i < board.length; i++) {
+    console.log(numToChar[i] + " |" + board[i].join(" "));
+  }
+};
